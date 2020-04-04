096e970d8b6ce1f97e3384a2d27e326c56bebbc8
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-04-04 17:07:00 -0600

    Express get and post methods

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+
+// para que express pueda interpretar el body
+const bodyParser = require("body-parser")
+// Peticiones
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+
 // const User = require("./models/User");
 
 // Para generar rutas hacia un archivo o carpeta 
@@ -29,9 +36,24 @@ app.listen(3000, () => {
 // En package.json puedes correr se agrego en script el start y ahora puedo usar 
 // npm start en la terminal en vez de correrlo directo
 
+
+// print
+function sayHi(req, res, next) {
+  console.log("Hola");
+  next();
+}
+
 // Query param
 // ? todo lo que va después es un query param y & para separar cada valor
-//users?minAge=30
-app.get("/users", (req, res) => {
+//users?minAge=30&maxAge=65&role=ADMIN
+app.get("/users", sayHi, (req, res) => {
   res.json(req.query);
-});
+}); 
+
+// Post
+app.post("/users", (req, res) => {
+  console.log(`Usuario: ${req.body.username} con password: ${req.body.password}`)
+ res.json(req.body)
+})
+
+
